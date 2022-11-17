ed2bf016824532c56268984c9e74b028614a1405
Guidzs <guidz.2004@gmail.com>
2022-11-17 14:18:45 -0300

    iniciando projeto

--- src/app.js
+++ src/app.js
@@ -3,9 +3,7 @@ const express = require('express');
 const app = express();
 
 // não remova esse endpoint, é para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
-});
+app.get('/', (_request, response) => response.send());
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
